4391ac06cfd775a7cddb431c79bda06eb1e45824
Neo <a0927024706@yahoo.com.tw>
2020-04-19 17:06:13 +0800

    without 0

--- password.py
+++ password.py
@@ -4,7 +4,10 @@ while x > 0 :
 	correct = input('請輸入密碼:')
 	if correct != password:
 		x = x-1
-		print('密碼錯誤!您還剩下', x,'次機會')
+		if x == 0:
+			print('密碼錯誤!!請稍後再試')
+		else:
+			print('密碼錯誤!您還剩下', x,'次機會')
 	else: 
 		print('您成功登入')
 		break
